7c8b16145aa756fb09fa2116a3cc17538062d1fa
Ankit Bhandari <ankeetbhandari77@gmail.com>
2026-07-05 01:37:19 +0545

    updated code

--- src/gsap.d.ts
+++ src/gsap.d.ts
@@ -1,4 +1,4 @@
-declare module "gsap-trial/SplitText" {
+declare module "gsap/SplitText" {
   export class SplitText {
     constructor(target: string | string[] | Element | Element[], vars?: Record<string, unknown>);
     chars: HTMLElement[];
@@ -9,7 +9,7 @@ declare module "gsap-trial/SplitText" {
   }
 }
 
-declare module "gsap-trial/ScrollSmoother" {
+declare module "gsap/ScrollSmoother" {
   export class ScrollSmoother {
     static create(vars?: Record<string, unknown>): ScrollSmoother;
     static refresh(hard?: boolean): void;
